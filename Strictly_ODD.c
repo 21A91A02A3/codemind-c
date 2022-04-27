0201019b502160532dcf09ba2bc691f73c74a7e7
21A91A02A3 <97613538+21A91A02A3@users.noreply.github.com>
2022-04-27 22:02:51 +0530

    #include<stdio.h> int main() {     int n,i,arr[n],count=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             if(i%2==0)             {                 count++;                 break;             }         }     }     if(count==0)     printf("True");     else     printf("False");     return 0; }

---
+++ Strictly_ODD.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[n],count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            if(i%2==0)
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    if(count==0)
+    printf("True");
+    else
+    printf("False");
+    return 0;
+}
